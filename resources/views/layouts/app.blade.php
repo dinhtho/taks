2c1ea909a9a0fdb974f392b3c5944f7c0c513c61
dinhtho <dinhthohcmus@gmail.com>
2021-05-13 18:08:52 +0700

    Udpate title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
     <html lang="en">
         <head>
-            <title>Laravel Quickstart - Basic</title>
+            <title>My tasks management</title>
 
             <!-- CSS And JavaScript -->
         </head>
